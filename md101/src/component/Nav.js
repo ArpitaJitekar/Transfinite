ec7725e18294d6c253b2d1ab18e249b7f0a355a6
ArpitaJitekar <165472438+ArpitaJitekar@users.noreply.github.com>
2024-10-20 12:44:42 +0530

    Update Nav.js

--- md101/src/component/Nav.js
+++ md101/src/component/Nav.js
@@ -1,11 +1,17 @@
 import React from 'react'
- import{Link} from "react-router-dom"
+ import{Link,useNavigate} from "react-router-dom"
 
 
 const Nav = () => {
- 
+  const navigate=useNavigate()
+  const logout=()=>{
+    localStorage.removeItem("user")
+    navigate('/signup')
+  }
+  const auth=localStorage.getItem('user')
   return (
     <div>
+
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark nav-css">
   <Link className="navbar-brand dash-css" to="/">Dashboard</Link>
   {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,11 +23,11 @@ const Nav = () => {
       <li className="nav-item active">
         <Link className="nav-link change-css" to="/form">Change the data</Link>
       </li></ul></div>
-      <div className="collapse navbar-collapse login-css mx-2" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <Link className="nav-link  " to="/login">Login</Link>
-      </li></ul></div>
+      {auth?<li className="nav-item active"><Link onClick={logout} class="nav-link" to="/">Logout</Link></li>:
+        <>
+        <li className="nav-link">
+        <Link className="Login nav-item " to="/login"> Login</Link>
+      </li></>}
 </nav>
     </div>
   )
