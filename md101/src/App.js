0496f3c7d5079383396e5fa8ffd060eb57af3c74
ArpitaJitekar <165472438+ArpitaJitekar@users.noreply.github.com>
2024-10-20 11:57:37 +0530

    Update App.js

--- md101/src/App.js
+++ md101/src/App.js
@@ -1,12 +1,58 @@
 
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import axios from 'axios';
+import { BrowserRouter,Routes ,Route} from 'react-router-dom';
+import Nav from "./component/Nav"
+import Form from "./component/Form"
+import Dashboard from "./component/Dashboard"
+const socket = io('http://localhost:5000'); // Connect to the backend WebSocket server
 
 function App() {
-  return (
-    <div>
+    const [data, setData] = useState([]);
+  const [playlist,setplaylist]=useState([]);
 
-    </div>
+    useEffect(() => {
+        // Listen for dataChange events from the server
+        socket.on('dataChange', (change) => {
+            console.log('Data change received:', change);
+            if (change.operationType === 'insert') {
+                setData((prevData) => [...prevData, change.fullDocument]);
+                setplaylist((prevplaylist)=>[...prevplaylist,change.fullDocument])
+            } else if (change.operationType === 'update') {
+                setData((prevData) =>
+                    prevData.map((item) =>
+                        item._id === change.documentKey._id ? { ...item, ...change.fullDocument } : item
+                    )
+                );
+            } else if (change.operationType === 'delete') {
+                setData((prevData) => prevData.filter((item) => item._id !== change.documentKey._id));
+            }
+        });
+
+        return () => {
+            socket.off('dataChange'); // Cleanup on unmount
+        };
+    }, []);
+
+    const addEvent = async (fname, lname, email) => {
+        const response = await axios.post('http://localhost:5000/changes',{fname, lname, email });
+        setData((prevData) => [...prevData, response.data]);
+        console.log('New event added:', response.data);
+    };
+
+    return (
+  <>
+   <BrowserRouter>
+   <Nav/>
+  <Routes>
+  <Route path='/' element={<Dashboard data={data} playlist={playlist}/>}/>
+  <Route path='/form' element={<Form addEvent={addEvent}/>}/>
+   </Routes>
+   
+    </BrowserRouter>
+    
+  </>
   );
 }
-
 export default App;
