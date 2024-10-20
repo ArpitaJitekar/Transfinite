5ff72ad50c2986438101c3b61fd207170a8dfc98
ArpitaJitekar <165472438+ArpitaJitekar@users.noreply.github.com>
2024-10-20 11:59:09 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,21 @@
 const express=require('express');
 const app=express();
-
+const cors=require('cors');
+const Mongodb=require('mongodb');
+const ws=require('ws');
+const sio=require('socket.io');
+const http=require('http');
+const server=http.createServer(app);
+const io=socketIo(server);
 const {faker}=require('@faker-js/faker');
+app.use(express.json())
+app.use(cors());
 const userIds=[];
 // or, if desiring a different locale
 // import { fakerDE as faker } from '@faker-js/faker';
 require('./db/configure');
+
+let loggedinuser;
 const user=require('./db/User');
 const music=require('./db/Music')
 const populateUsers=async()=>{
@@ -16,7 +26,7 @@ const email = faker.internet.email();
 const result=await user.create({
     fname,
     lname,
-    email,
+    email
 
 })
 
@@ -55,19 +65,72 @@ await music.create({
 user:randomuserId,
 songName,
 artistName,
-Genre,
+Genre,               
 Timespent,
 Date:date,
 Length:length
 })
  }
 }
+//user login
+app.post('/login',async(req,resp)=>{
+    const {email,password}=req.body
+    const user=await user.findOne({email})
+    if(!user){
+     return resp.send({error:"user not found"})
+    }
+    if(user.password==password){
+      console.log("login succesful");
+      loggedinuser=user.email;
+
+     }else{
+         resp.json({status:"error",message:"Invalid password"})
+     
+    }
+     
+ })
+ 
+//to send data from database to frontend
+//to send 
+app.get('/analytics',async(req,resp)=>{
+const result=await user.findOne({email:loggedinuser});
+resp.json(result);
+})
+//changestream to reflect changes
+// API endpoint for inserting a new document
+app.post('/changes', async (req, res) => {
+    const {fname,lname,email} = req.body;
+    const result = await user.insertOne({fname,lname,email});
+    res.status(201).json(result.ops[0]); // Return the inserted document
+});
+const setupchangestream=()=>{
+    // conn = new Mongodb("mongodb://localhost:27017");
+// db = conn.getDB('MD101');
+
+//     const collection=db.collection('user');
+//     const userChangeStream = collection.watch();
+const userChangeStream=user.watch()
+const musicstream=music.watch()
+    userChangeStream.on('change', (change) => {
+        io.emit('data changed',change);
+        
+        console.log('User Collection Change:', change);
+        //app.post('/')
+        // Here, you can emit an event to the frontend or update a dashboard in real-time
+    });
+    musicstream.on('change',(change)=>{
+        io.emit('data changed',change)
+    })
+
+}
 const populateDatabase = async () => {
     await populateUsers(); // Wait for users to be populated
     await populateSongs(); // Then populate songs
+    //set monogodb to replicatetype
+   setupchangestream();
 };
 populateDatabase().then(() => {
-    app.listen(5000, () => {
+    server.listen(5000, () => {
         console.log('Server is running on port 5000');
     });
 }).catch(err => {
